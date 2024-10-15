11c48823f2612d6dcc768076899747aec8ee33a1
Tamne2022002 <0306201074@caothang.edu.vn>
2024-10-16 00:37:35 +0700

    Create admin

---
+++ resources/views/admin/admin.blade.php
@@ -0,0 +1,22 @@
+@extends('admin.layout.head')
+@section('title')
+    <title>Trang Quản Trị</title>
+@endsection
+@section('css')
+    <link rel="stylesheet" href="{{ asset('/admins/css/style.css') }}">
+@endsection
+@section('content')
+    <div class="content-wrapper">
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row d-block">
+                    <div class="col-md-12 text-center d-flex justify-content-center align-items-center">
+                        <h2 class=" mt-5">
+                            <b>Chào mừng bạn đến với trang quản trị TL Bookstore</b>
+                        </h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
